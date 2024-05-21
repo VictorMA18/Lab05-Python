32708b26998f8ac171cb1f9f4e9d8b021b03745f
VictorMA18 <vmamanian@unsa.edu.pe>
2024-05-21 13:17:55 -0500

    Probando los Ejercicios de python

---
+++ CasoBase.py
@@ -0,0 +1,5 @@
+import pygame
+from chessPictures import *
+from interpreter import draw
+pygame.init()
+draw(knight)
---
+++ Ejercicio1.py
@@ -0,0 +1,5 @@
+import pygame
+from chessPictures import *
+from interpreter import draw
+pygame.init()
+iteraciones = [1,2,3,4]
