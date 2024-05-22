15e36733ab36e15ceec785a18381ae6e46a6f2b6
VictorMA18 <vmamanian@unsa.edu.pe>
2024-05-22 13:25:49 -0500

    Completando el ejercicio7 de la practica

--- Ejercicio7.py
+++ Ejercicio7.py
@@ -3,21 +3,21 @@ from chessPictures import *
 from interpreter import draw
 pygame.init()
 
+#OBJETOS NULOS
 tablabase = Picture("")
 board = Picture("")
 
+#FILAS NECESARIAS
 white_pawns = pawn.horizontalRepeat(8).up(square.join(square.negative()).horizontalRepeat(4))
 black_pawns = pawn.negative().horizontalRepeat(8).up(square.join(square.negative()).horizontalRepeat(4).negative())
-
 white_back_rank = rock.join(knight).join(bishop).join(queen).join(king).join(bishop).join(knight).join(rock).up(square.join(square.negative()).negative().horizontalRepeat(4))
-
 black_back_rank = white_back_rank.negative().up(square.join(square.negative()).horizontalRepeat(4))
+tablabase = ((square.join(square.negative()).horizontalRepeat(4)).under(square.join(square.negative()).negative().horizontalRepeat(4))).verticalRepeat(2)
 
-# Unión de filas para crear el tablero completo
+#BOARD 
 board = black_back_rank.under(board)
-tablabase = (square.join(square.negative()).horizontalRepeat(4)).under(square.join(square.negative()).negative().horizontalRepeat(4))
 board = board.under(black_pawns)
-board = board.under(tablabase.verticalRepeat(2))
+board = board.under(tablabase)
 board = board.under(white_pawns)
 board = board.under(white_back_rank)
 
